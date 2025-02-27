1a03cb40a49827edd68f20df8e8fcf8a35c38c9c
Christoffer Niska <christoffer.niska@codemate.com>
2025-02-27 11:53:43 +0200

    Move audio preloading

--- src/app.tsx
+++ src/app.tsx
@@ -1,27 +1,32 @@
 import { Stage } from "@pixi/react";
-import { AudioManager } from "./components/audio-manager";
+import { useEffect } from "react";
 import { GameScene } from "./components/game-scene";
 import { InputManager } from "./components/input-manager";
 import { STAGE_SIZE } from "./constants";
+import { useAudioStore } from "./stores/audio-store";
 
 export function App() {
   const [stageWidth, stageHeight] = STAGE_SIZE;
 
+  const preloadAudio = useAudioStore((state) => state.preloadAudio);
+
+  useEffect(() => {
+    preloadAudio();
+  }, [preloadAudio]);
+
   return (
-    <AudioManager>
-      <InputManager>
-        <Stage
-          width={stageWidth}
-          height={stageHeight}
-          options={{
-            backgroundColor: 0x1a1a1a,
-            antialias: true,
-            eventMode: "static",
-          }}
-        >
-          <GameScene />
-        </Stage>
-      </InputManager>
-    </AudioManager>
+    <InputManager>
+      <Stage
+        width={stageWidth}
+        height={stageHeight}
+        options={{
+          backgroundColor: 0x1a1a1a,
+          antialias: true,
+          eventMode: "static",
+        }}
+      >
+        <GameScene />
+      </Stage>
+    </InputManager>
   );
 }
